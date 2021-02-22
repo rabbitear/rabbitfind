eb1c6016b70badc5473247ed1600cc098bba6171
Jon Bradley <weatchu@gmail.com>
2021-02-21 18:14:21 -0900

    Add use of SelectEntry instead of just Entry

--- rabbitfind.go
+++ rabbitfind.go
@@ -1,10 +1,10 @@
 package main
 
 // TODO:
-// - Put the suggestions in labels under the rabbitEntry
+// - Put the suggestions in labels under the rabbitSelectEntry
 // - Use a regexp instead of all those strings.ReplaceAll's
 // BUG:
-// - could have less than sugslice[:10], in that for range loop.
+// - [fixed] could have less than sugslice[:10], in that for range loop.
 import (
 	"fmt"
 	"io/ioutil"
@@ -25,37 +25,37 @@ var baseUrl = "http://suggestqueries.google.com/complete/search?output=firefox&h
 var httpClient = &http.Client{Timeout: 10 * time.Second}
 var IsPrintable = regexp.MustCompile(`^[[:print:]]+$`).MatchString
 
-type rabbitEntry struct {
-	widget.Entry
+type rabbitSelectEntry struct {
+	widget.SelectEntry
 }
 
-func (e *rabbitEntry) onEsc() {
-	fmt.Println(e.Entry.Text)
+func (e *rabbitSelectEntry) onEsc() {
+	fmt.Println(e.SelectEntry.Text)
 	u, err := url.Parse("https://google.com/search?q=")
 	if err != nil {
 		log.Fatal(err)
 	}
 	q := u.Query()
-	q.Set("q", e.Entry.Text)
+	q.Set("q", e.SelectEntry.Text)
 	u.RawQuery = q.Encode()
 	fmt.Println(u)
 	a := app.New()
 	a.OpenURL(u)
-	e.Entry.SetText("")
+	e.SelectEntry.SetText("")
 }
 
-func newRabbitEntry() *rabbitEntry {
-	entry := &rabbitEntry{}
+func newRabbitSelectEntry() *rabbitSelectEntry {
+	entry := &rabbitSelectEntry{}
 	entry.ExtendBaseWidget(entry)
 	return entry
 }
 
-func (e *rabbitEntry) TypedKey(key *fyne.KeyEvent) {
+func (e *rabbitSelectEntry) TypedKey(key *fyne.KeyEvent) {
 	switch key.Name {
 	case fyne.KeyEscape:
 		e.onEsc()
 	default:
-		e.Entry.TypedKey(key)
+		e.SelectEntry.TypedKey(key)
 	}
 }
 
@@ -66,13 +66,14 @@ func main() {
 	box := container.NewVBox()
 	box.Add(widget.NewLabel("Press ESC to search"))
 
-	rabbitFindEntry := newRabbitEntry()
-	rabbitFindEntry.SetPlaceHolder("Type stuff here...")
+	rabbitFindSelectEntry := newRabbitSelectEntry()
+	rabbitFindSelectEntry.SetPlaceHolder("Type stuff here...")
 
-	rabbitFindEntry.OnChanged = func(s string) {
+	rabbitFindSelectEntry.OnChanged = func(s string) {
 		if !IsPrintable(s) {
 			return
 		}
+		// container for suggestions
 		s = strings.ReplaceAll(s, " ", "+")
 		res, err := httpClient.Get(baseUrl + s)
 		if err != nil {
@@ -84,19 +85,36 @@ func main() {
 			log.Println(err.Error())
 		}
 		tmpstr := string(body)
-		tmpstr = strings.ReplaceAll(tmpstr, "\"", "")
-		tmpstr = strings.ReplaceAll(tmpstr, "[", "")
-		tmpstr = strings.ReplaceAll(tmpstr, "]", "")
-		sugslice := strings.Split(tmpstr, ",")
+		sugslice := strings.FieldsFunc(tmpstr, func(r rune) bool {
+			switch r {
+			case '"':
+				return true
+			case '[':
+				return true
+			case ']':
+				return true
+			case ',':
+				return true
+			default:
+				return false
+			}
+		})
 
 		fmt.Printf("\n\n -=input> %s  -=len(sugslice)> %d\n", s, len(sugslice))
-		for i, value := range sugslice[:10] {
+		if len(sugslice) > 10 {
+			sugslice = sugslice[:10]
+		}
+		rabbitFindSelectEntry.SetOptions(sugslice)
+		for i, value := range sugslice {
 			fmt.Printf("i=%d:%s  ", i, value)
+			if i == 10 {
+				break
+			}
 		}
 	}
+	box.Add(rabbitFindSelectEntry)
 
-	box.Add(rabbitFindEntry)
-
+	w.Resize(fyne.NewSize(500, 420))
 	w.SetContent(box)
 	w.ShowAndRun()
 }
